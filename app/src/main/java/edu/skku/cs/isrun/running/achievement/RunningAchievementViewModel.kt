b52b2af06c0e566a558398cfb4f8cc889df553ec
justin639 <justin639@gmail.com>
2022-04-23 05:28:39 +0900

    Chore: RunningAchievementViewModel.kt typo in variables

--- app/src/main/java/edu/skku/cs/isrun/running/achievement/RunningAchievementViewModel.kt
+++ app/src/main/java/edu/skku/cs/isrun/running/achievement/RunningAchievementViewModel.kt
@@ -7,13 +7,16 @@ import androidx.lifecycle.ViewModel
 class RunningAchievementViewModel : ViewModel() {
     // TODO: Implement the ViewModel
     private var mText: MutableLiveData<String>? = null
-    private var recordAdapter: Achievement = Achievement(false, "title", "percent")
-    private var recordList = mutableListOf<Achievement>()
+    private var achievementTemp: Achievement = Achievement(false, "title", "percent")
+    private var achievementList = mutableListOf<Achievement>()
 
+    ///////////////////////////////////////////////////////////
     // Sample List
     private var sampleList = mutableListOf<Achievement>(Achievement(false, "100M!","50"),
         Achievement(true, "Your very first Run!", "98"),
         Achievement(false, "Too fast, Too Strong, Too good!", "13"))
+
+    ///////////////////////////////////////////////////////////
 
     fun RunningAchievementViewModel() {
         mText = MutableLiveData()
