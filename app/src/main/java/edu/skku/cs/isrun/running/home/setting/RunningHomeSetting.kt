a3b091fb8ba4ca876dad347e6cbb7e415bc42f62
justin639 <justin639@gmail.com>
2022-05-17 00:45:34 +0900

    Chore: Add Dynamic Recommendation Seek-Bar

--- app/src/main/java/edu/skku/cs/isrun/running/home/setting/RunningHomeSetting.kt
+++ app/src/main/java/edu/skku/cs/isrun/running/home/setting/RunningHomeSetting.kt
@@ -1,5 +1,6 @@
 package edu.skku.cs.isrun.running.home.setting
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SeekBar
+import android.widget.Switch
+import android.widget.TextView
 import androidx.navigation.Navigation
 import edu.skku.cs.isrun.R
 
@@ -17,6 +21,9 @@ class RunningHomeSetting : Fragment() {
     }
 
     private lateinit var viewModel: RunningHomeSettingViewModel
+    private var timeSet: Double = 0.0
+    private var distanceSet: Double = 0.0
+    private var freeMode: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,16 +36,91 @@ class RunningHomeSetting : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this)[RunningHomeSettingViewModel::class.java]
-        // TODO: Use the ViewModel
+
     }
 
+    @SuppressLint("SetTextI18n", "UseSwitchCompatOrMaterialCode")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setStartButton(view)
+        viewModel = ViewModelProvider(this)[RunningHomeSettingViewModel::class.java]
+        setStartButton(view,viewModel)
+
+        var freeMode = view.findViewById<Switch>(R.id.free_switch)
+        var recommendMode = view.findViewById<Switch>(R.id.recommend_switch)
+        val timeView = view.findViewById<TextView>(R.id.timeTitle)
+        val timePercent = view.findViewById<SeekBar>(R.id.seekBarTime)
+        val distanceView = view.findViewById<TextView>(R.id.distanceTitle)
+        val distancePercent = view.findViewById<SeekBar>(R.id.seekBarDistance)
+
+        freeMode.setOnCheckedChangeListener{ _, isChecked ->
+            viewModel.freeMode = isChecked
+        }
+        recommendMode.setOnCheckedChangeListener{ _, isChecked ->
+            viewModel.recommendMode = isChecked
+            if(viewModel.distanceSet < 24){
+                viewModel.recommendationRun(false)
+                timeView.text = "Time : ${viewModel.timeSet} min"
+                timePercent.progress = viewModel.getTimeProgress()
+            }else {
+                viewModel.recommendationRun(true)
+                distanceView.text = "Distance : ${viewModel.distanceSet} km"
+                distancePercent.progress = viewModel.getDistanceProgress()
+            }
+        }
+        // set text
+        timeView.text = "Time : ${viewModel.timeSet} min"
+        distanceView.text = "Distance : ${viewModel.distanceSet} km"
+        // set progress
+        timePercent.progress = viewModel.getTimeProgress()
+        distancePercent.progress = viewModel.getDistanceProgress()
+
+
+        timePercent.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if(p2){
+                    viewModel.getTime(p1)
+                    timeView.text = "Time : ${viewModel.timeSet} min"
+                    distanceView.text = "Distance : ${viewModel.distanceSet} km"
+                    distancePercent.progress = viewModel.getDistanceProgress()
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+        })
+        distancePercent.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                if(p2){
+                    viewModel.getDistance(p1)
+                    if(viewModel.distanceSet > 24.0 && viewModel.recommendMode) {
+                        viewModel.distanceSet = 24.0
+                        viewModel.timeSet = 180.0
+                        distancePercent.progress = viewModel.getDistanceProgress()
+                    }
+                    timeView.text = "Time : ${viewModel.timeSet} min"
+                    distanceView.text = "Distance : ${viewModel.distanceSet} km"
+                    timePercent.progress = viewModel.getTimeProgress()
+                }
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
     }
 
     // function for setting run button to next navigation
-    private fun setStartButton(view: View) {
+    private fun setStartButton(view: View,viewModel: RunningHomeSettingViewModel) {
+        freeMode = viewModel.freeMode
+        timeSet = viewModel.timeSet
+        distanceSet = viewModel.distanceSet
         val navController = Navigation.findNavController(view)
         view.findViewById<Button>(R.id.startBtn).setOnClickListener {
             navController.navigate(R.id.action_running_home_setting_to_running_home_running)
@@ -46,3 +128,5 @@ class RunningHomeSetting : Fragment() {
     }
 
 }
+
+
--- app/src/main/java/edu/skku/cs/isrun/running/home/setting/RunningHomeSettingViewModel.kt
+++ app/src/main/java/edu/skku/cs/isrun/running/home/setting/RunningHomeSettingViewModel.kt
@@ -1,7 +1,46 @@
 package edu.skku.cs.isrun.running.home.setting
 
 import androidx.lifecycle.ViewModel
+import kotlin.math.round
 
 class RunningHomeSettingViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    var freeMode = false
+    var recommendMode = true
+    var timeSet = 30.0
+    var distanceSet = 4.0
+
+
+    // average jogging : 8km/h | running 12km/h
+    fun recommendationRun(time_check:Boolean) {
+        if(time_check){
+            this.distanceSet = round(800.0/60.0*this.timeSet)/100
+        }else{
+            this.timeSet = round(this.distanceSet*6000.0/8.0)/100
+        }
+    }
+
+    fun getTimeProgress(): Int{
+        return ((this.timeSet-10.0)*100.0/170.0).toInt()
+    }
+
+    fun getDistanceProgress(): Int{
+        return ((this.distanceSet-1.0)*100.0/41.7).toInt()
+    }
+
+    fun getTime(timeProgress:Int){
+        this.timeSet = round((timeProgress.toDouble()*170.0/100.0 + 10.0)*100)/100
+        if(this.recommendMode){
+            recommendationRun(true)
+        }
+    }
+
+    fun getDistance(distanceProgress:Int){
+        this.distanceSet = round((distanceProgress.toDouble()*41.7/100.0 + 1.0)*100)/100
+        if(this.recommendMode){
+            recommendationRun(false)
+        }
+    }
+
+
 }
